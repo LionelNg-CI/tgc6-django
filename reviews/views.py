124a226abbfb70cf930d2c5ca1955bd1c924e87d
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-08-22 10:23:49 +0000

    added Book model database

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'books/index.template.html')
+    return render(request, 'reviews/index.template.html')
