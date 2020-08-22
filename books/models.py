9ebf64f612e2403b109df9cdf809f04012b0c484
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-08-22 11:20:01 +0000

    added read book.view route

--- books/models.py
+++ books/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Book(models.Model):
     title = models.CharField(blank=False, max_length=255)
     ISBN = models.CharField(blank=False, max_length=255)
@@ -9,10 +11,11 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
 class Author(models.Model):
-    First Name = models.CharField(blank=False, max_length=255)
-    Last Name = models.CharField(blank=False, max_length=255)
-    Date of Birth = models.DateField()
+    First_Name = models.CharField(blank=False, max_length=255)
+    Last_Name = models.CharField(blank=False, max_length=255)
+    Date_of_Birth = models.DateField()
 
     def __str__(self):
-        return self.title        
+        return self.First_Name
--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, 'books/index.template.html')
+    books = Book.objects.all()
+    return render(request, 'books/index.template.html', {'books':books})
